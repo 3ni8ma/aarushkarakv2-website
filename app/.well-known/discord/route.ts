b9d6778dff55d0888aeb5285bdf40c85adf47ee3
Aarush Karak <worldshaans@gmail.com>
2026-08-16 01:31:58 -0400

    revert: remove Discord domain verification endpoint

--- app/.well-known/discord/route.ts
+++
@@ -1,9 +0,0 @@
-export const dynamic = "force-static";
-
-export function GET() {
-  return new Response("dh=4254129523d9a434cd6a69129d93b4da08fe4a77", {
-    headers: {
-      "Content-Type": "text/plain; charset=utf-8",
-    },
-  });
-}
